fc3f45c77e6cd949e341c9f2b63bcf60df34ff6b
sljoai <99026795@qq.com>
2020-11-11 08:47:52 +0800

    添加方法异常堆栈字节码分析代码

---
+++ test4java/src/main/java/com/song/cn/jvm/bytecode/MyTest3.java
@@ -0,0 +1,24 @@
+package com.song.cn.jvm.bytecode;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class MyTest3 {
+
+    public void test(){
+        try {
+            FileInputStream inputStream = new FileInputStream("test.txt");
+            ServerSocket serverSocket = new ServerSocket(9099);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            System.out.println("finally");
+        }
+    }
+}
